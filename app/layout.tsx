632a95a422f0a51258270e5a4c1848cfabbc855c
Sav <sav01027@gmail.com>
2023-06-04 01:21:40 -0600

    Added metadata image

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,14 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
 	title: 'Sav Sidorov',
 	description: 'My personal website',
+	image: '/images/preview.png',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
 	return (
 		<html lang="en">
 			<body className={inter.className}>{children}</body>
