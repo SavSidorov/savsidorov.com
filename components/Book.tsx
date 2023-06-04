c2688de29a28fde7185804d4e3760c1b88c54d3a
Sav <sav01027@gmail.com>
2023-06-04 01:15:26 -0600

    Added AntD modals

--- components/Book.tsx
+++ components/Book.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Button, Modal } from 'antd';
 import styles from './Book.module.css';
 
 interface BookProps {
@@ -8,19 +9,55 @@ interface BookProps {
 		title: string;
 		subtitle: string;
 		author: string;
+		image: string;
 		review: string;
 		purchaseLink: string;
 		goodreadsLink: string;
-		image: string;
 	};
 }
 
 export default function Book({ book }: BookProps) {
+	const [loading, setLoading] = useState(false);
+	const [open, setOpen] = useState(false);
+
+	const showModal = () => {
+		setOpen(true);
+	};
+
+	const handleOk = () => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+			setOpen(false);
+		}, 3000);
+	};
+
+	const handleCancel = () => {
+		setOpen(false);
+	};
+
 	return (
 		<div className={styles.book}>
-			<Link href={book.goodreadsLink}>
-				<Image src={book.image} alt={book.title} width={165 / 1.2} height={250 / 1.2} />
-			</Link>
+			<Image
+				src={book.image}
+				alt={book.title}
+				width={165 / 1.2}
+				height={250 / 1.2}
+				onClick={showModal}
+			/>
+			<Modal
+				open={open}
+				title={book.title}
+				onOk={handleOk}
+				onCancel={handleCancel}
+				footer={null}
+				centered
+			>
+				<p>{book.review}</p>
+				<Link href={book.goodreadsLink}>Goodreads</Link>
+				<br />
+				<Link href={book.purchaseLink}>Purchase</Link>
+			</Modal>
 		</div>
 	);
 }
